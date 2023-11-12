858559d4d321f43d4f040c5312737ddbde346fa8
Patrick Fitzgerald <auskast03@gmail.com>
2023-11-12 03:12:44 -0800

    step 2 - user contact section

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,7 +7,7 @@ interface ContainerProps extends React.ComponentPropsWithoutRef<"div"> {}
 const Container = ({ className, ...props }: ContainerProps) => (
   <div
     className={clsx(
-      "w-[1122px] h-[667px] rounded-3xl bg-card shadow-xl",
+      "w-[1122px] h-[667px] rounded-[2rem] bg-card shadow-xl overflow-clip",
       className,
     )}
     {...props}
