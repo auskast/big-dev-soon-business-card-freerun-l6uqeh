eedf61c33191969717f6f3660d1ef4659dffa886
Patrick Fitzgerald <auskast03@gmail.com>
2023-11-13 00:13:28 -0800

    step 4 - user info (#4)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={clsx(
-          "bg-background body",
+          "bg-background text-black-87 body",
           poppinsRegular.className,
           poppinsRegular.variable,
           poppinsSemibold.variable,
---
+++ src/components/UserInfo.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+
+import clsx from "clsx";
+
+interface UserInfoProps extends React.ComponentPropsWithoutRef<"section"> {
+  firstName: string;
+  lastName: string;
+  title: string;
+  location: string;
+  description: string;
+  technologies: string[];
+}
+
+const UserInfo = ({
+  firstName,
+  lastName,
+  title,
+  location,
+  description,
+  technologies,
+  className,
+  ...props
+}: UserInfoProps) => (
+  <section
+    className={clsx(
+      "absolute top-[47px] left-[613px] p-10 flex flex-col gap-4",
+      className,
+    )}
+    {...props}
+  >
+    <h1 className="heading-lg">
+      <span className="block text-green-800">{firstName}</span>
+      <span className="block">{lastName}</span>
+    </h1>
+    <span className="heading-sm">
+      {title} · {location}
+    </span>
+    <p>{description}</p>
+    <span className="heading-sm">Working with technologies:</span>
+    <ul className="list-disc list-inside flex flex-col gap-3">
+      {technologies.map((tech) => (
+        <li key={tech} className="ml-2">
+          {tech}
+        </li>
+      ))}
+    </ul>
+  </section>
+);
+
+export { UserInfo };
+export type { UserInfoProps };
