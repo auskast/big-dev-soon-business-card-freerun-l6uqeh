b64b9ed84ea3ecc300e869b4f20e5909eea05762
Patrick Fitzgerald <auskast03@gmail.com>
2023-11-13 22:22:52 -0800

    disable image optimization and update base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+const basePath = process.env.BASE_PATH || undefined;
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+  basePath,
   output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
